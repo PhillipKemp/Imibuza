6553473ad844b4a7068710bd3e6c592405327251
phill <phill@DESKTOP-997PTHD>
2017-02-10 21:23:41 +0200

    Add project files.

---
+++ Imibuza.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Imibuza.Web.MvcApplication" Language="C#" %>
---
+++ Imibuza.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Imibuza.Web.Startup))]
+namespace Imibuza.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
