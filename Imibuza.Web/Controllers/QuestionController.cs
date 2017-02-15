3fa4cf405abeca151abadb0433cf350ccd594c79
PhillipKemp <phillipkemp007@gmail.com>
2017-02-16 01:58:49 +0200

    Going very strong. One UI element left and then data

---
+++ Imibuza.Web/Controllers/QuestionController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Imibuza.Web.Controllers
+{
+    public class QuestionController : Controller
+    {
+        // GET: Question
+        public JsonResult GetNext()
+        {
+            return new JsonResult();
+        }
+
+        public JsonResult SubmitAnswer()
+        {
+            return new JsonResult
+            {
+                Data = true
+            };
+        }
+
+
+    }
+}
--- Imibuza.Web/app/Imibuza.js
+++ Imibuza.Web/app/Imibuza.js
@@ -1,5 +1,11 @@
-﻿var app = angular.module("Imibuza", ["ngRoute"]);
-angular.module("ngMaterial", ["ng", "ngAnimate", "ngAria"]);
+﻿var app = angular.module("Imibuza",
+    [
+        "ngRoute",
+        "directives"
+    ]
+);
+
+
 
 app.config(function($routeProvider, $locationProvider) {
     $routeProvider.when("/", {
@@ -46,18 +52,96 @@ app.controller("CreateQuizController", function ($scope, $http) {
     ];
 
     $scope.addAnother = function () {
-        $http.post("Home/CreateQuestion", { model: $scope.newQuestion }).error(function (response) {
+        var result = $http.post("Home/CreateQuestion", { model: $scope.newQuestion }).then(function (response) {
+
+            var x = response;
 
         });
     }
 
     $scope.addOne = function () {
-        $http.post("Home/CreateQuestion", { model: $scope.newQuestion }).error(function (response) {
+        var result = $http.post("Home/CreateQuestion", { model: $scope.newQuestion }).then(function (response) {
+
+            var x = response;
 
         });
     }
+
+    $scope.init = function() {
+        
+    }
+
+    $scope.init();
 });
 
-app.controller("DoQuizController", function ($scope, $location) {
+app.controller("DoQuizController",
+    function ($scope, $location, $http) {
 
-});
+        $scope.quiz = {
+            category : "Politics",
+            question : "Who was the 10th US president?",
+            answers : [
+            { text: "Donald Trump", checked: false },
+            { text: "Aliens", checked: false },
+            { text: "Bill Bob Thornton", checked: false },
+            { text: "ALloy Wheels", checked: false }]
+        }
+
+        $scope.result = "";
+
+        $scope.reset = function() {
+            var result = $http.post("Question/GetNext", {}).then(function (response) {
+
+              
+
+            });
+        }
+
+        $scope.submitAnswer = function () {
+            var result = $http.post("Question/SubmitAnswer", {}).then(function (response) {
+                //if (response)
+                    //$scope.result = "Correct!";
+                //else
+                    //$scope.result = "Wrong!";
+            });
+        }
+
+        $scope.cancel = function() {
+            $location.path("/");
+        }
+
+        $scope.init = function () {
+            alert("init");
+        }
+
+        $scope.init();
+    });
+
+angular.module("directives", []).directive("selection",
+    function() {
+        return {
+            restrict: 'E',
+            scope: {
+                answer: '=',
+                source: '=',
+                submit: "&"
+            },
+
+            template: "<div class='option'>{{answer.text}}</div>",
+            link: function (scope, element, attrs) {
+                element.bind('click', function (e) {
+                    scope.submit();
+                });
+
+            },
+            controller: function($scope) {
+                //console.log($scope.answer.text);
+            }
+        }
+    });
+   
+
+//template = '<div  ng-click="$parent.toggleAnswers(answer)">{{answer.text}}</div>';
+
+
+
